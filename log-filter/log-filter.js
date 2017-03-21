9857501710de0ff53d2e5533b11479086171b985
Markus Laire <malaire@gmail.com>
2017-03-21 15:27:54 +0200

    refactoring: separate data grouping from output generation

--- log-filter/log-filter.js
+++ log-filter/log-filter.js
@@ -23,164 +23,166 @@
 const OUTPUT_SNIPPET_LIMIT = 12;
 const TRIM_LIMIT_UA        = 500;
 
-// ------=========---------------
-function GetOutput(data, limit) {
-// ------=========---------------
-  var data_start_time = data[0][0];
+// ------=========----------------
+function GetOutput(data_grouped) {
+// ------=========----------------
+  var data_start_time = (data_grouped.length == 0) ? NaN : data_grouped[0].ms_min;
 
   var text = '';
-  
-  var sum_count = 0;
-
-  var ms_first;
-  var uA_min, uA_max, uA_sum, uA_avg;
-  var mV_min, mV_max, mV_sum, mV_avg;
-  var nW_min, nW_max, nW_sum, nW_avg;
 
   var uA_div      = Number($('input[name=current-divisor]:checked').val());
   var mV_div      = Number($('input[name=voltage-divisor]:checked').val());
   var nW_div      = Number($('input[name=power-divisor]:checked').val());
-  var avg_count   = parseInt($('input[name=group-size]:checked').val());
+  var is_grouped  = (parseInt($('input[name=group-size]:checked').val()) > 1);
   var time_format = $('#time-format').val();
 
+  // CONVENTION: when not grouped, output 'minimum' value if any
+
   var include_time        = (time_format !== '');
-
-  var include_current     = ! isNaN(uA_div) && (avg_count == 1);
-  var include_voltage     = ! isNaN(mV_div) && (avg_count == 1);
-  var include_power       = ! isNaN(nW_div) && (avg_count == 1);
   
-  var include_current_min = ! isNaN(uA_div) && (avg_count > 1) && $('#current-min').prop('checked');
-  var include_current_avg = ! isNaN(uA_div) && (avg_count > 1) && $('#current-avg').prop('checked');
-  var include_current_max = ! isNaN(uA_div) && (avg_count > 1) && $('#current-max').prop('checked');
+  var include_current_min = ! isNaN(uA_div) && (! is_grouped || $('#current-min').prop('checked'));
+  var include_current_avg = ! isNaN(uA_div) &&    is_grouped && $('#current-avg').prop('checked');
+  var include_current_max = ! isNaN(uA_div) &&    is_grouped && $('#current-max').prop('checked');
 
-  var include_voltage_min = ! isNaN(mV_div) && (avg_count > 1) && $('#voltage-min').prop('checked');
-  var include_voltage_avg = ! isNaN(mV_div) && (avg_count > 1) && $('#voltage-avg').prop('checked');
-  var include_voltage_max = ! isNaN(mV_div) && (avg_count > 1) && $('#voltage-max').prop('checked');
+  var include_voltage_min = ! isNaN(mV_div) && (! is_grouped || $('#voltage-min').prop('checked'));
+  var include_voltage_avg = ! isNaN(mV_div) &&    is_grouped && $('#voltage-avg').prop('checked');
+  var include_voltage_max = ! isNaN(mV_div) &&    is_grouped && $('#voltage-max').prop('checked');
 
-  var include_power_min   = ! isNaN(nW_div) && (avg_count > 1) && $('#power-min').prop('checked');
-  var include_power_avg   = ! isNaN(nW_div) && (avg_count > 1) && $('#power-avg').prop('checked');
-  var include_power_max   = ! isNaN(nW_div) && (avg_count > 1) && $('#power-max').prop('checked');
+  var include_power_min   = ! isNaN(nW_div) && (! is_grouped || $('#power-min').prop('checked'));
+  var include_power_avg   = ! isNaN(nW_div) &&    is_grouped && $('#power-avg').prop('checked');
+  var include_power_max   = ! isNaN(nW_div) &&    is_grouped && $('#power-max').prop('checked');
   
   var current_unit = $('input[name=current-divisor]:checked').attr('data-unit');
   var voltage_unit = $('input[name=voltage-divisor]:checked').attr('data-unit');
   var power_unit   = $('input[name=power-divisor]:checked').attr('data-unit');
-  
-  var output_lines = 0;
 
   if ($('#column-labels').prop('checked')) {
     var row = [];
     
     if (include_time       ) row.push('time');
     
-    if (include_current    ) row.push(current_unit);
-    if (include_current_min) row.push(current_unit + ' min');
+    if (include_current_min) row.push(current_unit + (is_grouped ? ' min' : ''));
     if (include_current_avg) row.push(current_unit + ' avg');
     if (include_current_max) row.push(current_unit + ' max');
     
-    if (include_voltage    ) row.push(voltage_unit);
-    if (include_voltage_min) row.push(voltage_unit + ' min');
+    if (include_voltage_min) row.push(voltage_unit + (is_grouped ? ' min' : ''));
     if (include_voltage_avg) row.push(voltage_unit + ' avg');
     if (include_voltage_max) row.push(voltage_unit + ' max');
 
-    if (include_power      ) row.push(power_unit);
-    if (include_power_min  ) row.push(power_unit   + ' min');
+    if (include_power_min  ) row.push(power_unit   + (is_grouped ? ' min' : ''));
     if (include_power_avg  ) row.push(power_unit   + ' avg');
     if (include_power_max  ) row.push(power_unit   + ' max');
     
     text += row.join(',') + "\n";
   }
   
-  var skipped_some = false;
-  data.forEach(function (values, index) {
-    if (output_lines >= limit) {
-      skipped_some = true;
-      return;
+  data_grouped.forEach(function (input) {
+    var {count, ms_min,
+         uA_min, uA_max, uA_sum,
+         mV_min, mV_max, mV_sum,
+         nW_min, nW_max, nW_sum} = input;
+
+    var time;
+    if ($('#time-from-zero').prop('checked')) {
+      ms_min -= data_start_time;
+      time = (new moment.utc(ms_min, 'x')).format(time_format);
+    } else {
+      time = (new moment(ms_min, 'x')).format(time_format);
     }
+
+    var row = [];
+
+    if (include_time       ) row.push(time);
+
+    if (include_current_min) row.push(uA_min / uA_div);
+    if (include_current_avg) row.push(uA_sum / count / uA_div);
+    if (include_current_max) row.push(uA_max / uA_div);
+
+    if (include_voltage_min) row.push(mV_min / mV_div);
+    if (include_voltage_avg) row.push(mV_sum / count / mV_div);
+    if (include_voltage_max) row.push(mV_max / mV_div);
+
+    if (include_power_min  ) row.push(nW_min / nW_div);
+    if (include_power_avg  ) row.push(nW_sum / count / nW_div);
+    if (include_power_max  ) row.push(nW_max / nW_div);
     
-    var ms = values[0];
-    var uA = values[1];
-    var mV = values[2];
+    text += row.join(',') + "\n";
+  });
+  
+  return text;
+}
+
+// ------=========---------------------------
+function GroupData(data_parsed, limit = -1) {
+// ------=========---------------------------
+  var data_grouped = [];
+  var group_size = parseInt($('input[name=group-size]:checked').val());
+
+  var count = 0;
+  var ms_min;
+  var uA_min, uA_max, uA_sum;
+  var mV_min, mV_max, mV_sum;
+  var nW_min, nW_max, nW_sum;
+
+  for (var index = 0; index < data_parsed.length; index++) {
+    var [ms, uA, mV] = data_parsed[index];
     var nW = uA * mV;
-    
-    if (avg_count > 1) {
-      if (sum_count == 0) {
-        ms_first = ms;
-        uA_min   = 1e15;
-        uA_max   = -1e15;
-        uA_sum   = 0;
-        mV_min   = 1e15;
-        mV_max   = -1e15;
-        mV_sum   = 0
-        nW_min   = 1e15;
-        nW_max   = -1e15;
-        nW_sum   = 0
-      }
-      
-      sum_count++;
+
+    if (count == 0) {
+      ms_min = ms;
+      uA_min = uA_max = uA_sum = uA;
+      mV_min = mV_max = mV_sum = mV;
+      nW_min = nW_max = nW_sum = nW;
+    } else {
       uA_sum += uA;
       mV_sum += mV;
       nW_sum += nW;
-      
+
       if (uA < uA_min) uA_min = uA;
       if (uA > uA_max) uA_max = uA;
       if (mV < mV_min) mV_min = mV;
       if (mV > mV_max) mV_max = mV;
       if (nW < nW_min) nW_min = nW;
       if (nW > nW_max) nW_max = nW;
-      
-      if (sum_count < avg_count &&
-          ! (index + 1 == data.length &&
-             $('#include-last-partial').prop('checked'))) return;
-      
-      ms = ms_first;
-      uA_avg = uA_sum / sum_count;
-      mV_avg = mV_sum / sum_count;
-      nW_avg = nW_sum / sum_count;
-      
-      sum_count = 0;
     }
-    
-    output_lines++;
+    count++;
 
-    var time
-    if ($('#time-from-zero').prop('checked')) {
-      ms = ms - data_start_time;
-      time = (new moment.utc(ms, 'x')).format(time_format);
-    } else {
-      time = (new moment(ms, 'x')).format(time_format);
+    if (count == group_size) {
+      data_grouped.push({
+        'count' : count,
+        'ms_min': ms_min,
+        'uA_min': uA_min, 'uA_max': uA_max, 'uA_sum': uA_sum,
+        'mV_min': mV_min, 'mV_max': mV_max, 'mV_sum': mV_sum,
+        'nW_min': nW_min, 'nW_max': nW_max, 'nW_sum': nW_sum,
+      });
+      count = 0;
+      if (data_grouped.length == limit) break;
     }
+  };
 
-    var row = [];
-    
-    if (include_time       ) row.push(time);
-    
-    if (include_current    ) row.push(uA     / uA_div);
-    if (include_current_min) row.push(uA_min / uA_div);
-    if (include_current_avg) row.push(uA_avg / uA_div);
-    if (include_current_max) row.push(uA_max / uA_div);
+  if (count > 0) {
+    data_grouped.push({
+      'count' : count,
+      'ms_min': ms_min,
+      'uA_min': uA_min, 'uA_max': uA_max, 'uA_sum': uA_sum,
+      'mV_min': mV_min, 'mV_max': mV_max, 'mV_sum': mV_sum,
+      'nW_min': nW_min, 'nW_max': nW_max, 'nW_sum': nW_sum,
+    });
+  }
 
-    if (include_voltage    ) row.push(mV     / mV_div);
-    if (include_voltage_min) row.push(mV_min / mV_div);
-    if (include_voltage_avg) row.push(mV_avg / mV_div);
-    if (include_voltage_max) row.push(mV_max / mV_div);
+  // possibly exclude last
+  if (! $('#include-last-partial').prop('checked')
+      && data_grouped.slice(-1)[0].count < group_size) {
+    data_grouped.pop();
+  }
 
-    if (include_power      ) row.push(nW     / nW_div);
-    if (include_power_min  ) row.push(nW_min / nW_div);
-    if (include_power_avg  ) row.push(nW_avg / nW_div);
-    if (include_power_max  ) row.push(nW_max / nW_div);
-    
-    text += row.join(',') + "\n";
-  });
-  
-  if (skipped_some) text += "...\n";
-  
-  return text;
+  return data_grouped;
 }
 
 // ------==========----
 function ParseInput() {
 // ------==========----
-  var data = [];
+  var data_parsed = [];
   
   var trimming_beginning = $('#trim-begin').prop('checked');
   
@@ -198,24 +200,22 @@ function ParseInput() {
       trimming_beginning = false;
     }
     
-    data.push([ ms, uA, mV ]);
+    data_parsed.push([ ms, uA, mV ]);
   });
   
-  return data;
+  return data_parsed;
 }
 
 // ------==============----
 function ShowFullOutput() {
 // ------==============----
-  var data = ParseInput();
-  var text = GetOutput(data);
-  $('#output-snippet').val(text);
+  var data_grouped = GroupData(ParseInput());
+  $('#output-snippet').val(GetOutput(data_grouped));
 }
 
 // ------===================----
 function UpdateOutputSnippet() {
 // ------===================----
-  var data = ParseInput();
-  var text = GetOutput(data, OUTPUT_SNIPPET_LIMIT);
-  $('#output-snippet').val(text);
+  var data_grouped = GroupData(ParseInput(), OUTPUT_SNIPPET_LIMIT);
+  $('#output-snippet').val(GetOutput(data_grouped) + "...\n");
 }
